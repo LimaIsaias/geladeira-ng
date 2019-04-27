dd870d7b4631286df759d41aa15a029397be719a
LimaIsaias <coelholimaisaias@gmail.com>
2019-04-26 22:45:16 -0300

    Implementated the Produto component

--- src/app/produtos/produtos.component.ts
+++ src/app/produtos/produtos.component.ts
@@ -1,12 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { Produto } from '../produto';
+import { Component, OnInit, ModuleWithComponentFactories } from '@angular/core';
+import * as moment from 'moment';
+import { Produto } from '../model/produto';
 
 @Component({
   selector: 'app-produtos',
   templateUrl: './produtos.component.html',
 })
 export class ProdutosComponent implements OnInit {
-  produtos: Produto[];
+   compra: Date =  new Date(2019, 4, 26) ;
+   validade: Date  =  new Date(2019, 8, 26) ;
+  produtos: Produto[] = [
+    { id: '1',
+      descricao: 'Leite',
+      preco: 10.99,
+      dataCompra : this.compra,
+      categoria : 'Laticínios',
+      validade: this.validade,
+      imagePath: 'assets/img/restaurants/tasty.png'
+    },
+    { id: '2',
+      descricao: 'Pão de Forma',
+      preco: 4.99,
+      dataCompra : this.compra,
+      categoria : 'Pães',
+      validade: this.validade,
+      imagePath: 'assets/img/restaurants/breadbakery.png'
+    }];
 
   constructor() { }
   ngOnInit() {
